a2c1b428bfa9a6034071431054b774dd93360ff5
pemilytech <vakadu10@gmail.com>
2024-02-22 08:49:02 +0530

    added more problems

--- recursion/23-sub-sequence.js
+++ recursion/23-sub-sequence.js
@@ -6,10 +6,27 @@ function subsequence(s, r) {
     }
 
     let ch = s.charAt(0);
+    let ascii = s.charCodeAt(0);
 
     subsequence(s.slice(1), r + ch);
-    subsequence(s.slice(1), r)
-
+    subsequence(s.slice(1), r);
+    subsequence(s.slice(1), r + ascii)
 }
 
-subsequence("ab", "");
+function subsequence2(s, r) {
+    if(s === "") {
+        let arr = [];
+        arr.push(r);
+        return arr;
+    }
+
+    let ch = s.charAt(0);
+
+    let left = subsequence2(s.slice(1), ch + r);
+    let right = subsequence2(s.slice(1), r);
+
+    return [...left, ...right]
+}
+
+subsequence("abc", "")
+// console.log(subsequence2("abc", ""));
