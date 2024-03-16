5e0d9599450a7f0992f110c03fb1d6d658af4283
vakadu <vakadu10@gmail.com>
2024-03-16 19:39:43 +0530

    added double ll

---
+++ puzzles/unique-paths-2.js
@@ -0,0 +1,27 @@
+function uniquePaths(grid) {
+    return helper(grid, 0, 0, "");
+}
+
+function helper(g, r, c, p) {
+    let m = g.length - 1, n = g[0].length - 1, count = 0;
+
+    if(g[r][c]) {
+        return 0;
+    }
+
+    if(r === m && c === n) {
+        return 1
+    }
+
+    if(r<m && !g[r + 1][c]) {
+        count += helper(g, r+1, c, p + "D");
+    }
+
+    if(c<n && !g[r][c+1]) {
+        count += helper(g, r, c+1, p + "R");
+    }
+
+    return count
+}
+
+console.log(uniquePaths([[0,1]]));
