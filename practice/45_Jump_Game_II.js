810ab144092d8079d04f3c11ce9f2278b0b265e4
Vakadu Vinod <vakadu10@gmail.com>
2025-01-06 20:30:49 +0530

    more

---
+++ practice/45_Jump_Game_II.js
@@ -0,0 +1,24 @@
+function jumpGame(nums) {
+    return helper(nums, 0, {});
+}
+
+function helper(nums, i, memo) {
+    if (i in memo) {
+        return memo[i];
+    }
+
+    if (i >= nums.length - 1) {
+        return 0;
+    }
+
+    let min = Number.MAX_SAFE_INTEGER;
+
+    for (let j = 1; j <= nums[i]; j++) {
+        min = Math.min(min, 1 + helper(nums, i + j, memo));
+    }
+
+    memo[i] = min;
+    return min;
+}
+
+console.log(jumpGame([2, 3, 1, 1, 4]));
